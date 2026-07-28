1ae08be8e795cd166cc0bc7d30337fa9100e317c
Smeltier <connect.goncalves@gmail.com>
2026-07-27 22:28:23 -0300

    att: adjust indentation in CpfTest.java file

--- backend/src/test/java/br/com/delivery/domain/shared/CpfTest.java
+++ backend/src/test/java/br/com/delivery/domain/shared/CpfTest.java
@@ -6,37 +6,37 @@ import static org.junit.jupiter.api.Assertions.*;
 import br.com.delivery.domain.exception.InvalidCpfValueException;
 
 public class CpfTest {
-  @Test
-  void shouldNormalizeValue() {
-    String value = "955.999.370-46";
-    String normalizedValue = "95599937046";
+    @Test
+    void shouldNormalizeValue() {
+        String value = "955.999.370-46";
+        String normalizedValue = "95599937046";
 
-    var cpf = new Cpf(value);
+        var cpf = new Cpf(value);
 
-    assertEquals(normalizedValue, cpf.value());
-  }
+        assertEquals(normalizedValue, cpf.value());
+    }
 
-  @Test
-  void shouldThrowWhenValueIsASequence() {
-    String value = "111.11.111-11";
+    @Test
+    void shouldThrowWhenValueIsASequence() {
+        String value = "111.11.111-11";
 
-    assertThrows(InvalidCpfValueException.class,
-        () -> new Cpf(value));
-  }
+        assertThrows(InvalidCpfValueException.class,
+                () -> new Cpf(value));
+    }
 
-  @Test
-  void shouldThrowWhenValueIsBlank() {
-    String value = "";
+    @Test
+    void shouldThrowWhenValueIsBlank() {
+        String value = "";
 
-    assertThrows(InvalidCpfValueException.class,
-        () -> new Cpf(value));
-  }
+        assertThrows(InvalidCpfValueException.class,
+                () -> new Cpf(value));
+    }
 
-  @Test
-  void shouldThrowWhenValueDontMatchWithPattern() {
-    String value = "955.999-370.46";
+    @Test
+    void shouldThrowWhenValueDontMatchWithPattern() {
+        String value = "955.999-370.46";
 
-    assertThrows(InvalidCpfValueException.class,
-        () -> new Cpf(value));
-  }
+        assertThrows(InvalidCpfValueException.class,
+                () -> new Cpf(value));
+    }
 }
