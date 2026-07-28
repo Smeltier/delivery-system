c706265321ed1f8fd201d9c3d59dcb533f047f66
Smeltier <connect.goncalves@gmail.com>
2026-07-27 22:02:46 -0300

    add: implementation for Cnpj value object

--- backend/src/main/java/br/com/delivery/domain/shared/Cnpj.java
+++ backend/src/main/java/br/com/delivery/domain/shared/Cnpj.java
@@ -1,4 +1,18 @@
 package br.com.delivery.domain.shared;
 
-public record Cnpj(String value) {   
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+import br.com.delivery.domain.exception.InvalidCnpjValueException;;
+
+public record Cnpj(String value) {
+    private static final Pattern CNPJ_PATTERN = Pattern.compile("^\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}$");
+
+    public Cnpj {
+        value = Objects.requireNonNull(value, "CNPJ should be not null.");
+
+        if (!CNPJ_PATTERN.matcher(value).matches()) {
+            throw new InvalidCnpjValueException("Invalid CNPJ format.");
+        }
+    }
 }
