141c6f25f91cdb7eccdd5bb914e1d7f7fa282e58
Smeltier <connect.goncalves@gmail.com>
2026-08-03 16:26:02 -0300

    fix: .trim()

--- backend/src/main/java/br/com/delivery/domain/shared/ZipCode.java
+++ backend/src/main/java/br/com/delivery/domain/shared/ZipCode.java
@@ -3,29 +3,28 @@ package br.com.delivery.domain.shared;
 import java.util.regex.Pattern;
 
 public record ZipCode(String value) {
-  // G: maybe this isn't right.
-  private static final Pattern BRAZIL_ZIP_PATTERN = Pattern.compile("\\d{5}-?\\d{3}");
+    private static final Pattern BRAZIL_ZIP_PATTERN = Pattern.compile("\\d{5}-?\\d{3}");
 
-  public ZipCode {
-    if (value == null || value.isBlank()) {
-      throw new IllegalArgumentException("CEP inválido.");
+    public ZipCode {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("CEP inválido.");
+        }
+
+        value = value.trim();
+
+        if (!BRAZIL_ZIP_PATTERN.matcher(value).matches()) {
+            throw new IllegalArgumentException("CEP inválido.");
+        }
+
+        value = normalize(value);
     }
 
-    value.trim();
-
-    if (!BRAZIL_ZIP_PATTERN.matcher(value).matches()) {
-      throw new IllegalArgumentException("CEP inválido.");
+    private String normalize(String value) {
+        return value.replace("-", "");
     }
 
-    value = normalize(value);
-  }
-
-  private String normalize(String value) {
-    return value.replace("-", "");
-  }
-
-  @Override
-  public String toString() {
-    return value.substring(0, 5) + "-" + value.substring(5);
-  }
+    @Override
+    public String toString() {
+        return value.substring(0, 5) + "-" + value.substring(5);
+    }
 }
