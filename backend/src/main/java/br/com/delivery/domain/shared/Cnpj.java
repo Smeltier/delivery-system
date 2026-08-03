86d0a4b1b8bac3583f59b62002818b490dcbab54
Smeltier <connect.goncalves@gmail.com>
2026-08-03 17:35:59 -0300

    att: remove duplicated parenthesis

--- backend/src/main/java/br/com/delivery/domain/shared/Cnpj.java
+++ backend/src/main/java/br/com/delivery/domain/shared/Cnpj.java
@@ -3,7 +3,7 @@ package br.com.delivery.domain.shared;
 import java.util.Objects;
 import java.util.regex.Pattern;
 
-import br.com.delivery.domain.exception.InvalidCnpjValueException;;
+import br.com.delivery.domain.exception.InvalidCnpjValueException;
 
 public record Cnpj(String value) {
     private static final Pattern CNPJ_PATTERN = Pattern.compile("^\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}$");
