c4b71879b15437a8ba86a5de94a1416c5220568a
Smeltier <connect.goncalves@gmail.com>
2026-08-09 16:44:26 -0300

    add(infra): add IClientRepository JDBC implementation

---
+++ backend/src/main/java/br/com/delivery/infrastructure/persistence/jdbc/ClientJdbcRepository.java
@@ -0,0 +1,108 @@
+package br.com.delivery.infrastructure.persistence.jdbc;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import br.com.delivery.domain.repositories.IClientRepository;
+import br.com.delivery.domain.account.AccountId;
+import br.com.delivery.domain.client.Client;
+import br.com.delivery.domain.shared.Address;
+import br.com.delivery.domain.shared.Cpf;
+import br.com.delivery.domain.shared.ZipCode;
+
+@Repository
+public class ClientJdbcRepository implements IClientRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ClientJdbcRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Optional<Client> findById(AccountId id) {
+        String sql = """
+                SELECT c.id, c.cpf, c.address_id,
+                a.street, a.number, a.complement, a.city, a.country, a.zip_code
+                FROM clients c
+                LEFT JOIN addresses a ON a.id = c.address_id
+                WHERE c.id = ?
+                """;
+        List<Client> result = jdbcTemplate.query(sql, (rs, rowNum) -> mapRow(rs), id.value());
+
+        if (result.isEmpty()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(result.get(0));
+    }
+
+    @Override
+    public boolean existsById(AccountId id) {
+        String sql = "SELECT COUNT(*) FROM clients WHERE id = ?";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, id.value());
+        return count != null && count > 0;
+    }
+
+    @Override
+    public void save(Client client) {
+        UUID addressId = null;
+
+        if (client.getAddress() != null) {
+            addressId = UUID.randomUUID();
+            Address address = client.getAddress();
+
+            String addressQuery = "INSERT INTO addresses (id, street, number, complement, city, country, zip_code) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            jdbcTemplate.update(
+                    addressQuery,
+                    addressId,
+                    address.street(),
+                    address.number(),
+                    address.complement(),
+                    address.city(),
+                    address.country(),
+                    address.zipCode().value());
+        }
+
+        String cpfValue = client.getCpf().map(Cpf::value).orElse(null);
+
+        String sql = """
+                INSERT INTO clients (id, cpf, address_id)
+                VALUES (?, ?, ?)
+                ON CONFLICT (id) DO UPDATE
+                SET cpf = EXCLUDED.cpf, address_id = EXCLUDED.address_id
+                """;
+
+        jdbcTemplate.update(sql, client.getId().value(), cpfValue, addressId);
+    }
+
+    private Client mapRow(ResultSet rs) throws SQLException {
+        AccountId id = new AccountId(UUID.fromString(rs.getString("id")));
+        Client client = Client.restore(id);
+
+        String cpfValue = rs.getString("cpf");
+        if (cpfValue != null) {
+            client.setCpf(new Cpf(cpfValue));
+        }
+
+        Address address = null;
+        if (rs.getObject("address_id") != null) {
+            address = new Address(
+                    rs.getString("street"),
+                    rs.getString("number"),
+                    rs.getString("complement"),
+                    rs.getString("city"),
+                    rs.getString("country"),
+                    new ZipCode(rs.getString("zip_code")));
+
+            client.updateAddress(address);
+        }
+
+        return client;
+    }
+}
