c3aa1ad79c4101b435f33c6c29077bd488578c50
Smeltier <connect.goncalves@gmail.com>
2026-08-11 11:35:37 -0300

    att(infra): implements findById and save methods

--- backend/src/main/java/br/com/delivery/infrastructure/persistence/jdbc/RestaurantOwnerJdbcRepository.java
+++ backend/src/main/java/br/com/delivery/infrastructure/persistence/jdbc/RestaurantOwnerJdbcRepository.java
@@ -1,12 +1,20 @@
 package br.com.delivery.infrastructure.persistence.jdbc;
 
-import br.com.delivery.domain.account.AccountId;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import br.com.delivery.domain.repositories.IRestaurantOwnerRepository;
 import br.com.delivery.domain.restaurantowner.RestaurantOwner;
-import org.springframework.jdbc.core.JdbcTemplate;
+import br.com.delivery.domain.account.AccountId;
+import br.com.delivery.domain.shared.Cnpj;
 
-import java.util.Optional;
-
+@Repository
 public class RestaurantOwnerJdbcRepository implements IRestaurantOwnerRepository {
     private final JdbcTemplate jdbcTemplate;
 
@@ -16,7 +24,14 @@ public class RestaurantOwnerJdbcRepository implements IRestaurantOwnerRepository
 
     @Override
     public Optional<RestaurantOwner> findById(AccountId id) {
-        return Optional.empty();
+        String sql = "SELECT id, corporate_name, cnpj FROM restaurant_owners WHERE id = ?";
+        List<RestaurantOwner> result = jdbcTemplate.query(sql, (rs, rowNum) -> mapRow(rs), id.value());
+
+        if (result.isEmpty()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(result.get(0));
     }
 
     @Override
@@ -28,5 +43,29 @@ public class RestaurantOwnerJdbcRepository implements IRestaurantOwnerRepository
 
     @Override
     public void save(RestaurantOwner restaurantOwner) {
+        String sql = """
+                INSERT INTO restaurant_owners (id, corporate_name, cnpj)
+                VALUES(?, ?, ?)
+                ON CONFLICT (id) DO UPDATE
+                SET corporate_name = EXCLUDED.corporate_name, cnpj = EXCLUDED.cnpj
+                """;
+
+        jdbcTemplate.update(
+                sql,
+                restaurantOwner.getId().value(),
+                restaurantOwner.getCorporateName(),
+                restaurantOwner.getCnpj().value());
+    }
+
+    private RestaurantOwner mapRow(ResultSet rs) throws SQLException {
+        AccountId id = new AccountId(UUID.fromString(rs.getString("id")));
+        String corporateName = rs.getString("corporate_name");
+
+        String cnpjValue = rs.getString("cnpj");
+        if (cnpjValue != null) {
+            return RestaurantOwner.restore(id, new Cnpj(cnpjValue), corporateName);
+        }
+
+        return RestaurantOwner.restore(id, null, corporateName);
     }
 }
